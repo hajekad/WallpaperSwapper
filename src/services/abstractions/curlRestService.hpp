652d066efea60a3be50619da4a5fd0a5ed11a7ce
hajekad3 <hajekad3@fit.cvut.cz>
2023-02-22 04:45:07 +0100

    curl ret is in variable

--- src/services/abstractions/curlRestService.hpp
+++ src/services/abstractions/curlRestService.hpp
@@ -2,18 +2,23 @@
 
 #include <curl/curl.h>
 #include <string>
+#include <cstring>
 #include <iostream>
 
 #include "../../interface/exceptions/NetworkException.cpp"
+
+size_t dataSize = 0;
 
 class CurlRestService
 {
     CURL * curl;
     CURLcode res;
     std::string hdUrl;
-    
+
 public:
     CurlRestService();
+
+    void getInfo();
     
     void getNewWallpaperUrl();
 };
--- src/services/implementations/curlRestService.cpp
+++ src/services/implementations/curlRestService.cpp
@@ -4,6 +4,21 @@ CurlRestService::CurlRestService() {
     curl = curl_easy_init();
     curl_global_init(CURL_GLOBAL_ALL);
     hdUrl = "";
+    dataSize = 0;
+}
+
+size_t curlWriteFunction(void* ptr, size_t size, size_t nmemb, void* userdata) {
+    char** stringToWrite=(char**)userdata;
+    const char* input=(const char*)ptr;
+    if(nmemb==0) return 0;
+    if(!*stringToWrite)
+        *stringToWrite = (char *) malloc(nmemb+1);
+    else
+        *stringToWrite = (char *) realloc(*stringToWrite, dataSize+nmemb+1);
+    memcpy(*stringToWrite+dataSize, input, nmemb);
+    dataSize+=nmemb;
+    (*stringToWrite)[dataSize]='\0';
+    return nmemb;
 }
 
 void CurlRestService::getNewWallpaperUrl() {
@@ -11,8 +26,12 @@ void CurlRestService::getNewWallpaperUrl() {
         throw NetworkException("Could not instantiate curl service.");
     }
 
+    char * data = NULL;
+
     curl_easy_setopt(curl, CURLOPT_URL, "https://api.nasa.gov/planetary/apod?api_key=3uALHm6PeNktClBFYS2DQgTi1cYM1FoYCzwI9SGa");
-    
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &curlWriteFunction);
+
     CURLcode res = curl_easy_perform(curl);
 
     std::cout << std::endl << res << std::endl;
@@ -22,4 +41,6 @@ void CurlRestService::getNewWallpaperUrl() {
     }
 
     curl_easy_cleanup(curl);
+
+    std::cout << data << std::endl;
 }
